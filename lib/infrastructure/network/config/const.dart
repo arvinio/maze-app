c008a8d046f9cb6571fda1d1df9248545b078c24
ZaraProgrammer <zahra.tafazoli@gmail.com>
2024-06-04 01:29:19 +0330

    second commit

---
+++ lib/infrastructure/network/config/const.dart
@@ -0,0 +1,4 @@
+class Const {
+  static const String mazeBaseUrl = '';
+  static const int networkTimeout = 60000;
+}
---
+++ lib/infrastructure/network/config/strings.dart
@@ -0,0 +1,5 @@
+class Strings {
+  static const String internetError = '';
+  static const String serverError = '';
+  static const String generalError = '';
+}
