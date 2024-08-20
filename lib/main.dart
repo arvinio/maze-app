e1f0d27d9c5ab84ebcf7ce1627974bd267fa52b6
ZaraProgrammer <zahra.tafazoli@gmail.com>
2024-08-21 00:51:29 +0330

    sentry added to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:maze_app/core/util/shared_preferences_helper.dart';
 
 import 'core/presentation/app.dart';
 import 'di/injection_container.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sentry_logging/sentry_logging.dart';
+import 'package:logging/logging.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -11,9 +17,18 @@ Future<void> main() async {
   await Globals.setSharedPreferences();
 
   configureDependencies(environment: 'dev');
-
-  runApp(App(
-    appRouter: inject(),
-    appTheme: inject(),
-  ));
+    const dsn =
+        'https://d18530a5bcef0dacb949a11a71803aff@o4507613419929600.ingest.us.sentry.io/4507613422551040';
+    await SentryFlutter.init(
+            (options) {
+              options.dsn =kDebugMode ? '' : dsn;
+          options.diagnosticLevel = SentryLevel.info;
+          options.tracesSampleRate = 1.0;
+          options.addIntegration(LoggingIntegration(minEventLevel: Level.INFO));
+        },
+        appRunner: () =>
+            runApp(App(
+              appRouter: inject(),
+              appTheme: inject(),
+            )));
 }
